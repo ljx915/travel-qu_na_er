b06fc6d7a4f98525c57aaabe78c66a96a54765a6
Lijianxiong8 <1293964560@qq.com>
2019-09-03 21:46:24 +0800

    add search to city

--- src/components/City.vue
+++ src/components/City.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="city"> 
     <city-header></city-header>
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
 import CityHeader from '@/components/city/Header.vue'
+import CitySearch from '@/components/city/Search.vue'
 
 // export default导出一个对象
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,CitySearch
   }
 }
 </script>
---
+++ src/components/city/Search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="search"> 
+      <input class="search-input" type="text" placeholder="请输入城市名或拼音">
+  </div>
+</template>
+
+<script>
+// export default导出一个对象
+export default {
+  name: 'CitySearch',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/varibles.styl'
+    
+  .search
+    height .72rem
+    padding 0 .1rem
+    background $bgColor
+    .search-input 
+      box-sizing border-box
+      padding 0 .1rem
+      width 100%
+      height .62rem
+      line-height .62rem
+      text-align center
+      border-radius .06rem
+      color #666
+</style>
