cf44c4ac2b039f1a6285ddbe5eead4c9302bd1c6
Lijianxiong8 <1293964560@qq.com>
2019-09-10 23:00:58 +0800

    header finish(animation+activated+deactivated)

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,16 +1,72 @@
 <template>
   <div class="detail"> 
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+
   </div>
 </template>
 
 <script>
 import DetailBanner from './detail/Banner.vue'
+import DetailHeader from './detail/Header.vue'
 // export default导出一个对象
 export default {
   name: 'Deatil',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
